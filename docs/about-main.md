424db6999529385e306e729dee29f88174e29ab9
Ad Heinz <Heinzad@users.noreply.github.com>
2025-12-30 13:59:25 +1300

    Main entry point

---
+++ docs/about-main.md
@@ -0,0 +1,6 @@
+<!-- docs\about-main.md -->
+
+Main
+====
+
+The main module provides an entry point to the proejct. 
---
+++ main.py
@@ -0,0 +1,10 @@
+# src\main.py
+
+"""main entry point for the project"""
+
+
+# ensure code is only executed when the script is run directly, not when imported as a module
+    
+if __name__ == "__main__":
+
+    print("hello world!")
